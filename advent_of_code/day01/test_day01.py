b6f4f4a375250cf4ab88ea015942aea1f2cecf65
Iain Samuel McLean Elder <iain@isme.es>
2023-12-02 21:28:07 +0100

    Test on all day 1 samples

--- advent_of_code/day01/test_day01.py
+++ advent_of_code/day01/test_day01.py
@@ -2,19 +2,34 @@ import sys
 from pathlib import Path
 from unittest.mock import patch
 
+import pytest
 from pytest import CaptureFixture
 
 import advent_of_code.day01.part1
 import advent_of_code.day01.part2
 
 
-def test_day01_part1(capsys: CaptureFixture[str]) -> None:
-    with patch.object(sys, "argv", ["test_part1", "advent_of_code/day01/input"]):
+@pytest.mark.parametrize(
+    "document,value",
+    [
+        ("advent_of_code/day01/input/part1_sample", "142\n"),
+        ("advent_of_code/day01/input/puzzle", "55447\n"),
+    ],
+)
+def test_solve_part1(capsys: CaptureFixture[str], document: str, value: str) -> None:
+    with patch.object(sys, "argv", ["test_part1", document]):
         advent_of_code.day01.part1.main()
     out, err = capsys.readouterr()
-    assert out == "55447\n"
+    assert out == value
 
 
-def test_day01_part2() -> None:
-    solution = advent_of_code.day01.part2.Solution(Path("advent_of_code/day01/input"))
-    assert solution.calibration_sum() == 54706
+@pytest.mark.parametrize(
+    "document,value",
+    [
+        ("advent_of_code/day01/input/part2_sample", 281),
+        ("advent_of_code/day01/input/puzzle", 54706),
+    ],
+)
+def test_solve_part2(document: str, value: int) -> None:
+    solution = advent_of_code.day01.part2.Solution(Path(document))
+    assert solution.calibration_sum() == value
